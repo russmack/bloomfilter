6bc51b26c7324ce3c903b6a2510f3f387cea133b
russ <russ_mack@hotmail.com>
2017-07-28 19:43:07 +0100

    Integrate bit array.

--- bloomfilter.go
+++ bloomfilter.go
@@ -9,9 +9,10 @@
 package bloomfilter
 
 import (
-	"fmt"
 	"hash/fnv"
 	"math"
+
+	"github.com/russmack/bitarray-go"
 )
 
 // Hash32Fn is a function type for 32 bit hashing functions.
@@ -19,44 +20,18 @@ type Hash32Fn func(string) uint32
 
 // BloomFilter is the public struct.
 type BloomFilter struct {
-	Filter       []byte
-	Size         uint32
-	HashFuncs    []Hash32Fn
-	TotalFlipped uint32
+	filter       *bitarraygo.BitArray
+	size         uint32
+	hashFuncs    []Hash32Fn
+	totalFlipped uint32
 }
 
-func (b *BloomFilter) SetTrue(i uint32) {
-	bitByte := byteFromBitIndex(i)
-	i = offsetBitIndex(i)
-	b.Filter[bitByte] |= 1 << i
+func (b *BloomFilter) setTrue(i uint32) {
+	b.filter.Set(uint64(i), true)
 }
 
-func (b *BloomFilter) Get(i uint32) bool {
-	bitByte := byteFromBitIndex(i)
-	r := b.Filter[bitByte]
-
-	i = offsetBitIndex(i)
-	isSet := r & (1 << i)
-
-	if isSet != 0 {
-		return true
-	}
-	return false
-}
-
-// byteFromBitIndex returns the index in the []byte of the byte
-// which contains the specified bit.
-func byteFromBitIndex(i uint32) uint32 {
-	relevantByte := i / 8
-	return relevantByte
-}
-
-// offsetBitIndex returns the index of the specified bit in its byte of the []byte.
-func offsetBitIndex(i uint32) uint32 {
-	if i >= 8 {
-		i = i % 8
-	}
-	return i
+func (b *BloomFilter) get(i uint32) bool {
+	return b.filter.Get(uint64(i))
 }
 
 // NewBloomFilter creates a new BloomFilter with the specified number of switches,
@@ -64,10 +39,10 @@ func offsetBitIndex(i uint32) uint32 {
 // when checking for existence.
 func NewBloomFilter(filterSize uint32) *BloomFilter {
 	b := BloomFilter{}
-	b.Filter = make([]byte, filterSize/8+1)
-	b.Size = filterSize
+	b.filter = bitarraygo.NewBitArray(4294967295)
+	b.size = filterSize
 	// TODO: inject preferred choice of hashers.
-	b.HashFuncs = []Hash32Fn{hashFnv1, hashFnv1a}
+	b.hashFuncs = []Hash32Fn{hashFnv1, hashFnv1a}
 	return &b
 }
 
@@ -90,21 +65,17 @@ func hashFnv1a(s string) uint32 {
 // getIndex32 hashes a string, then reduces that hash to an index within the bounds of the filter.
 func (b *BloomFilter) getIndex32(s string, hashFn Hash32Fn) uint32 {
 	h := hashFn(s)
-	return h % b.Size
+	return h % b.size
 }
 
 // Add a string to the Bloom filter.
 func (b *BloomFilter) Add(s string) {
-	fmt.Println("\nAdding:", s)
 	// Iterate over the list of hash functions, using each to reduce the string
 	// to a single index in the filter, which is then flipped on.
-	for i, j := range b.HashFuncs {
+	for _, j := range b.hashFuncs {
 		idx := b.getIndex32(s, j)
-		fmt.Println("Hash function #", i, "hashed to index:", idx)
-		b.SetTrue(idx)
-
-		fmt.Println("Updated filter to:", b.Filter)
-		b.TotalFlipped++
+		b.setTrue(idx)
+		b.totalFlipped++
 	}
 }
 
@@ -113,10 +84,10 @@ func (b *BloomFilter) Exists(s string) bool {
 	// Put the candidate string through each of the hash functions, and for each
 	// index returned get the value at that index in the bloom filter, and put
 	// those values into the results slice.
-	results := make([]bool, len(b.HashFuncs))
-	for i, j := range b.HashFuncs {
+	results := make([]bool, len(b.hashFuncs))
+	for i, j := range b.hashFuncs {
 		idx := b.getIndex32(s, j)
-		results[i] = b.Get(idx)
+		results[i] = b.get(idx)
 	}
 
 	allTrue := true
@@ -133,8 +104,8 @@ func (b *BloomFilter) Exists(s string) bool {
 // divided by the length of the filter,
 // to the power of the number of hash functions.
 func (b *BloomFilter) GetFalsePositiveProbability() float64 {
-	x := float64(b.TotalFlipped) / float64(b.Size)
-	y := float64(len(b.HashFuncs))
+	x := float64(b.totalFlipped) / float64(b.size)
+	y := float64(len(b.hashFuncs))
 	z := math.Pow(x, y) * 100
 	p := math.Floor(z + .5)
 	return p
